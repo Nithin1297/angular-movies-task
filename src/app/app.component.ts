ff85da951b303ce3d9c33cc5fcb21f94134817a0
Nithin1297 <nithin.madduri@proclink.com>
2024-07-24 09:38:14 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { CardComponent } from "./card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
-  title = 'my-first-project';
+ 
 }
